058eb1e96b143cdf3510d898c5d8e66f1de2375c
Eduardo Marcelino <eduardo.marcelino.eng@gmail.com>
2025-01-14 15:49:11 -0300

    Footer component created

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,41 @@
+<script>
+
+</script>
+
+
+<style scoped>
+    footer {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        background-color: #24231E;
+        padding: 1rem 0;
+
+        @media (min-width: 600px) {
+            padding: 0;
+        }
+
+        p {
+            text-align: center;
+            color: white;
+
+            a {
+                color: var(--color-primary);
+            }
+
+            @media (min-width: 600px) {
+                br {
+                    display: none;
+                }
+            }
+
+            margin: 0;
+        }
+    }
+</style>
+
 <template>
-    <div>
-        Footer
-    </div>
+    <footer>
+        <p>Desenvolvido por <a target="_blank" href="https://eduardomarcelinodev.com.br/meus-projetos">Eduardo Marcelino</a> © 2025. <br> Todos os direitos reservados.</p>
+    </footer>
 </template>
